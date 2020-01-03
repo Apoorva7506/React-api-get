319bc6300541a429e26ad3ddaa1562a0d5864000
unknown <apoorvaflame@gmail.com>
2020-01-03 11:40:45 +0530

    1st commit

---
+++ apiget/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Contacts from "./components/contacts";
+
+class App extends Component {
+  state = {
+    contacts: []
+  };
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ contacts: data });
+      });
+  }
+
+  render() {
+    return <Contacts contacts={this.state.contacts} />;
+  }
+}
+
+export default App;
